64031e3bc58f0837ae1af87f0c774b4e466f41d2
Robert <robkhz@umich.edu>
2024-10-12 15:11:51 -0400

    Base app

---
+++ RideWatch/CreateAccount.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from 'react'
+
+const CreateAccount = () => {
+  return (
+    <View>
+        <TextInput>Create Account</TextInput>
+    </View>
+  )
+}
+
+export default CreateAccount
---
+++ RideWatch/HomePage.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import CreateAccount from './CreateAccount'
+
+const HomePage = () => {
+  return (
+    <View>
+        <Text>Home page!</Text>
+        <CreateAccount />
+    </View>
+  )
+}
+
+export default HomePage
