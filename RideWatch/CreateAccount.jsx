dc90eaa8bef945b6b089fdbd586c403dfd2d4de3
Robert <robkhz@umich.edu>
2024-10-12 16:33:17 -0400

    Basic homepage skeleton

--- RideWatch/CreateAccount.jsx
+++ RideWatch/CreateAccount.jsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Switch, Pressable, Button } from "react-native";
+import React, {useState} from 'react'
+import Styles from "./styles";
 
 const CreateAccount = () => {
+
+    const [parentAccount, setParentAccount] = useState(false);
+
   return (
     <View>
-        <TextInput>Create Account</TextInput>
+        <TextInput style={Styles.loginBoxes} placeholder="Username" />
+        <TextInput style={Styles.loginBoxes} placeholder="Password" secureTextEntry />
+        <View>
+            <Text>Parent Account?</Text>
+            <Switch value={parentAccount} onValueChange={() => setParentAccount(!parentAccount)} />
+        </View>
+
+        <Button style={Styles.submitBox} title="Create Account" />
     </View>
   )
 }
---
+++ RideWatch/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+  loginBoxes: {
+    margin: 0,
+  },
+  submitBox: {
+    color: "blue",
+  },
+  boxText: {
+    color: "white",
+  },
+});
+
+export default Styles;
