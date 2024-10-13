bd41979f9bacefeff3aa9d5653866f7f9ef5e761
Robert <robkhz@umich.edu>
2024-10-12 20:34:00 -0400

    Env config

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -1,22 +1,22 @@
 from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+from dotenv import load_dotenv
+from os import environ
 import MySQLdb.cursors
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:8081"}})
 
-# # MySQL Configuration (update with your own credentials)
-# app.config["MYSQL_HOST"] = "localhost"  # Use server IP if remote
-# app.config["MYSQL_USER"] = "youruser"
-# app.config["MYSQL_PASSWORD"] = "yourpassword"
-# app.config["MYSQL_DB"] = "yourdatabase"  # Database name from MySQL Workbench
-# app.config["MYSQL_CURSORCLASS"] = (
-#     "DictCursor"  # Optional: returns results as dictionary
-# )
+# MySQL Configuration (update with your own credentials)
+app.config["MYSQL_HOST"] = environ.get("MYSQL_HOST", "localhost")
+app.config["MYSQL_USER"] = environ.get("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = environ.get("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = environ.get("MYSQL_DB")
+app.config["MYSQL_CURSORCLASS"] = ("DictCursor") #optional returning as dict
 
-# # Initialize MySQL connection
-# mysql = MySQL(app)
+# Initialize MySQL connection
+mysql = MySQL(app)
 
 @app.route("/createAccount", methods=["GET"])
 def validate_user():
